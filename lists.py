1f0dae76e23a60147dae0257e2280eb828a47df6
Krishna <bkesari@outlook.com>
2020-06-03 19:09:17 -0400

    commit

--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-Creating  a list
+# Creating  a list
 
 x = 1; y = 2; z = 3
 list1 = [x,y,z]
@@ -8,11 +8,26 @@ list2[1] = 4
 
 print(list1)
 
-# Creating expression from list
+# Creating expression from list (list comprehensions)
 l = [2,4,8,16]
 print([i ** 3 for i in l])
 
 # Creating composite functions
 
-x = 1
-print(x)
+items = [["rice", 2.4, 8], ["flour", 1.9, 5], ["corn", 4.7, 6]]
+for item in items:
+    print("Product: %s Price: %.2f Quality: %i" % (item[0], item[1], item[2]))
+
+# Bracket Operations
+items[1][1] = items[1][1] * 1.2
+print(items[1][1])
+
+# list comprehension
+def f1(x): return x*2 
+def f2(x): return x*4 
+
+lst = [] 
+for i in range(16): 
+    lst.append(f1(f2(i))) 
+print(lst) 
+print([f1(x)  for x in range(64) if x in [f2(j) for j in range(16)]]) 
