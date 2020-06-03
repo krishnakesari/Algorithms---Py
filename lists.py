d4b1df4122b91c7c94974d9fbc036f1f7facf790
Krishna <bkesari@outlook.com>
2020-06-03 19:03:32 -0400

    commit

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+Creating  a list
+
 x = 1; y = 2; z = 3
 list1 = [x,y,z]
 
@@ -5,3 +7,12 @@ list2 = list1
 list2[1] = 4
 
 print(list1)
+
+# Creating expression from list
+l = [2,4,8,16]
+print([i ** 3 for i in l])
+
+# Creating composite functions
+
+x = 1
+print(x)
