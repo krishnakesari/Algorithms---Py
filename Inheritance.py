d0947e5b331dfb12e43f46abe5d1632325d82875
Krishna <bkesari@outlook.com>
2020-06-07 17:53:35 -0400

    commit

--- Inheritance.py
+++ Inheritance.py
@@ -1,6 +1,8 @@
-## Modifying behavior of an existing class through inheritance
+
+# Modifying behavior of an existing class through inheritance
 class specialEmployee(Employee):
     def hours(self, numHours):
         self.own += numHours * self.rate * 2
         return("%.2f hours worked" % numHours)
+
 
