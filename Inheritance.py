ff0898e71777f7e7002cd39d7e21dfb81714d691
Krishna <bkesari@outlook.com>
2020-06-07 18:02:56 -0400

    commit

--- Inheritance.py
+++ Inheritance.py
@@ -5,4 +5,24 @@ class specialEmployee(Employee):
         self.own += numHours * self.rate * 2
         return("%.2f hours worked" % numHours)
 
+## For a sub-class to define a new class vairable, it needs __init__() method:
+class specialEmployee(Employee): 
+    def __init__(self,name,rate, bonus): 
+        Employee.__init__(self, name, rate) #calls the base classes 
+        self.bonus = bonus 
 
+    def hours(self, numHours): 
+        self.owed += numHours * self.rate + self.bonus  
+        return("%.2f hours worked" % numHours)  
+
+    
+class Aexp(object): 
+    base=2 
+    @classmethod 
+    def exp(cls,x): 
+        return(cls.base**x) 
+
+class Bexp(Aexp): 
+    base=3 
+
+BxSqr.sqr(3)
