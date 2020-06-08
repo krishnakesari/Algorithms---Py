98b65a40dc7275b006a1db02806cae3d3d317b20
Krishna <bkesari@outlook.com>
2020-06-08 17:13:29 -0400

    commit

--- Data Types & Structures.py
+++ Data Types & Structures.py
@@ -1,2 +1,12 @@
 # Operations and expressions
+# 1. None Type
+
+# 2. Boolean
+## a. not x (Returns True if x is false otherwise)
+## b. x and y (Return True if x and y are true)
+## c. x or y
+
+# 3. Integer, float, or complex zero
+# 4. Empty sequence or mapping
+# 5. User defined classes __len__() or __bool__()
 
