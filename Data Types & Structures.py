7f28061eb3fa11f5f29b903ec2f2319de34c587e
Krishna <bkesari@outlook.com>
2020-06-08 18:02:56 -0400

    commit

--- Data Types & Structures.py
+++ Data Types & Structures.py
@@ -1,5 +1,5 @@
 # Operations and expressions
-# 1. None Type
+# 1. None Type - immutable - often used as a default value in optional arguments to allow functions to detect whether the caller passed a value.
 
 # 2. Boolean
 ## a. not x (Returns True if x is false otherwise)
@@ -23,4 +23,42 @@ print(x == y) # Equivalence
 print(x is y)  # Object Identity
 x = y  # Assignment
 print(x is y) 
+
+# Representation Error
+print(1-0.9)
+print(1-0.9 == 0.1)
+
+# Precision / Rounding
+import decimal
+x = decimal.Decimal(3.14);
+y = decimal.Decimal(2.74)
+print(x * y)
+decimal.getcontext().prec = 4 # Precision 
+print(x * y)
+
+# Handling Fractions
+import fractions
+print(fractions.Fraction(3,4))
+print(fractions.Fraction(0.5))
+print(fractions.Fraction("0.25"))
+
+# Sequences
+## Ordered sets of objects indexed by non-negative integers
+## List and Tuples - sequences of arbitrary objects
+print(tuple('sequence'))
+## strings - sequences of characters
+## string, tuple and range objects are immutable (only return a value rather than actually change the value)
+
+## len(s) - number of elements
+## min, max, sum, all, any 
+## s + r, s * n,
+## s[i] - Indexing returns elements i of s
+## s[i:j:stride] - Slicing returns elements between i and j with optional stride
+##  x in s (Returns True if element x is in s), x not in s
+
+l = ['one', 'two']
+x, y = l
+x, y = y, x
+print(y,x)
+
 
