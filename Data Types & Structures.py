182e21b4188867cc7773d1e1d50882ab7372c58e
Krishna <bkesari@outlook.com>
2020-06-08 17:28:09 -0400

    commit

--- Data Types & Structures.py
+++ Data Types & Structures.py
@@ -11,4 +11,16 @@
 # 5. User defined classes __len__() or __bool__()
 
 # Comparision and Arithmetic Operators
+## + - * /  // quotient (3 // 2 returns 1)
+## x ** y (exponent)
+## x % y (remainder)
+## < <= > >= == !=
+
+
+# Membership, identity and logical operations
+x = [1,2,3];  y = [1,2,3]
+print(x == y) # Equivalence
+print(x is y)  # Object Identity
+x = y  # Assignment
+print(x is y) 
 
