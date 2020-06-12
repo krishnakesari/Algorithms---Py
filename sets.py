a87ac3d399d4ab43861e4db6e7a9c4d4172901c9
Krishna <bkesari@outlook.com>
2020-06-12 06:09:40 -0400

    commit

--- sets.py
+++ sets.py
@@ -13,3 +13,15 @@ print('ab' in s1) # Testing for a member's presence in the set
 # Lopping through elements in a set
 for element in s1: 
     print(element)
+
+# Frozen or Immutable sets
+s1.add(frozenset(s2))
+print(s1)
+
+# Using frozen set as a key to a dictionary
+fs1 = frozenset(s1)
+fs2 = frozenset(s2)
+{fs1: 'fs1', fs2: 'fs2'}
+
+
+
