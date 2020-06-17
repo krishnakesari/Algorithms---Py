67dcb12729707b5b5a0832204eb4f40c1c323f65
Krishna <bkesari@outlook.com>
2020-06-17 17:14:11 -0400

    commit

--- Named Tuples.py
+++ Named Tuples.py
@@ -1,5 +1,14 @@
 from collections import namedtuple
 
 space = namedtuple('space', 'x y z')
-s1 = space(x = 2.0, y = 4.0, z = 10)
-print(s1.x * s1.y * s1.z) # Calculating volume
+sl = space(x = 2.0, y = 4.0, z = 10)
+print(sl.x * sl.y * sl.z) # Calculating volume
+
+space2 = namedtuple('space2', 'x def z', rename= True)
+sl = space2(3,4,5)
+
+print(sl._1)
+
+sl = [4, 5, 6]
+print(space._make(sl))
+
