e2e41f043fe9b62137d64549f67122c43f190daa
Krishna <bkesari@outlook.com>
2020-06-20 07:54:47 -0400

    Algotihms

--- Algorithm Design.py
+++ Algorithm Design.py
@@ -34,3 +34,55 @@ def bitStr(n,s):
     return [digit + bits for digit in bitStr(1,s) for bits in bitStr(n-1,s)]
 
 print(bitStr(3,'abc'))
+
+# Karatsuba Algorithm (Long Multiplication)
+## Steps
+## 1. Build a recursive algorithm
+## 2. Split into two halves (first half most significant digits, second half least significant digits)
+
+from math import log10
+
+def karatsuba(x,y): 
+
+    # The base case for recursion 
+    if x < 10 or y < 10: 
+        return x*y     
+
+    #sets n, the number of digits in the highest input number 
+    n = max(int(log10(x)+1), int(log10(y)+1)) 
+
+    # rounds up n/2     
+    n_2 = int(math.ceil(n / 2.0)) 
+    #adds 1 if n is uneven 
+    n = n if n % 2 == 0 else n + 1 
+
+    #splits the input numbers      
+    a, b = divmod(x, 10**n_2) 
+    c, d = divmod(y, 10**n_2) 
+
+    #applies the three recursive steps 
+    ac = karatsuba(a,c) 
+    bd = karatsuba(b,d) 
+    ad_bc = karatsuba((a+b),(c+d)) - ac - bd 
+
+    #performs the multiplication     
+    return (((10**n)*ac) + bd + ((10**n_2)*(ad_bc))) 
+
+print(karatsuba(15,14))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
